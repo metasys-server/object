dcd040c4176ba8be82d58c28673356774b162cc1
Michael Welch <michael.g.welch@jci.com>
2018-08-27 18:38:21 -0500

    Fix #3 If login is unsuccesful exit.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
-const _ = require('@jci/serverkit');
+const _ = require('@serverkit/serverkit');
 const { promptPassword, processArguments } = require('./lib/helpers');
 
 const { MetasysServerApi, Reference } = _;
 
 async function main() {
-  const password = await promptPassword();
+  const password = promptPassword();
   const options = processArguments();
   const reference = new Reference(options.reference);
 
@@ -16,7 +16,11 @@ async function main() {
 
 
   const api = new MetasysServerApi();
-  await api.login(options.username, password, options.hostname, options.options);
+  const successful = await api.login(options.username, password, options.hostname, options.options);
+
+  if (!successful) {
+    return;
+  }
 
   const device = await _.first(api.supervisoryDevices(),
     element => element.itemReference === reference.engineReference);
